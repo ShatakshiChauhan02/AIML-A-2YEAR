8a2b5997e802da8a78a20e8ee49d6a0b68dbf184
ShatakshiChauhan02 <shatakshichauhanabes28@gmail.com>
2026-07-29 13:52:09 +0530

    JS Basics

--- Day 1/index.js
+++ Day 1/index.js
@@ -5,8 +5,23 @@
 // console.log(a+b+c);
 
 
+// var x=25;
+// if(x==20)
+// {
+//     console.log("x is 20");
+// }
+// else
+//     console.log("x is not 20")
+
+// for(var i=0;i<5;i++)
+//  {
+//     console.log(i);
+//  }
+
 var x=20;
-if(x==20)
-{
-    console.log("x is 20");
-}
+const arr=[1,2,3,4,5];
+for(var i=0;i<arr.length;i++)
+ {
+    console.log(arr[i]);
+    console.log(arr[2]);
+ }
