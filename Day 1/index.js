13fd98293e41b565d06c0de89c32658b3fc7b442
ShatakshiChauhan02 <shatakshichauhanabes28@gmail.com>
2026-07-29 13:58:31 +0530

    JS Basics

--- Day 1/index.js
+++ Day 1/index.js
@@ -1,4 +1,4 @@
-// const app= require('./app.js')
+const app= require('./app.js');
 // var a=20;
 // var b=20;
 // var c=20;
@@ -18,10 +18,13 @@
 //     console.log(i);
 //  }
 
-var x=20;
-const arr=[1,2,3,4,5];
-for(var i=0;i<arr.length;i++)
- {
-    console.log(arr[i]);
-    console.log(arr[2]);
- }
+// var x=20;
+// const arr=[1,2,3,4,5];
+// for(var i=0;i<arr.length;i++)
+//  {
+//     console.log(arr[i]);
+//     console.log(arr[2]);
+//  }
+
+ console.log(app.xz);
+  console.log(app.yy);
