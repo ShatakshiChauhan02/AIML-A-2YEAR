0d269c302a594d2837e376c71c3d408acfa08449
ShatakshiChauhan02 <shatakshichauhanabes28@gmail.com>
2026-07-29 13:41:02 +0530

    Variable

--- Day 1/index.js
+++ Day 1/index.js
@@ -1,12 +1,12 @@
 // const app= require('./app.js')
-var a=20;
-var b=20;
-var c=20;
-console.log(a+b+c);
+// var a=20;
+// var b=20;
+// var c=20;
+// console.log(a+b+c);
 
 
-// var x=20;
-// if(x==20)
-// {
-//     console.log("x is 20");
-// }
+var x=20;
+if(x==20)
+{
+    console.log("x is 20");
+}
