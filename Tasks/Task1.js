55b38bb4fa6211198739c9dcd5920d40da5b5308
ShatakshiChauhan02 <shatakshichauhanabes28@gmail.com>
2026-08-14 11:03:56 +0530

    Tasks 1 and 2

---
+++ Tasks/Task1.js
@@ -0,0 +1,9 @@
+let Name = "Shatakshi";
+let id = "2503215300180";
+let Branch = "CSE";
+let Department = "AIML";
+
+console.log("Name:", Name);
+console.log("ID:", id);
+console.log("Branch:", Branch);
+console.log("Department:", Department);
