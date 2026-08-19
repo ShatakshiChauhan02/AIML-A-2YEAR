c7efe4114ea35eaec4146f1039dfaae7aae37a1c
ShatakshiChauhan02 <shatakshichauhanabes28@gmail.com>
2026-08-19 13:39:12 +0530

    String

---
+++ Day 3/Array.js
@@ -0,0 +1,42 @@
+console.log("==== String Object ====");
+
+let name = "Shatakshi";
+console.log(name);
+
+//String Length
+
+console.log("Length = ",name.length);
+
+//--------------------------------
+// String Methods
+//--------------------------------
+
+console.log("\n==== String Methods ====");
+
+let str="Welcome JavaScript";
+
+//1.toUpperCase()
+
+console.log("Upper Case = ",str.toUpperCase());
+
+//2.toLowerCase()
+
+console.log("Lower Case = ",str.toLowerCase());
+
+//3. charAt()
+
+console.log("Character at inder 3 = ",str[2]);
+
+//4. indexOf()
+
+console.log("Index of Java = ",str.indexOf("Java"));
+
+//5. lastIndex()
+
+let message = "JavaScript is easy. JavaScript is powerful";
+
+console.log(
+    "Last Index = ",
+    message.lastIndexOf("JavaScript")
+);
+
