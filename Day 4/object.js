9063cf04a3c1d6b514df4f586580ca7be0d33aa9
ShatakshiChauhan02 <shatakshichauhanabes28@gmail.com>
2026-09-02 14:24:51 +0530

    Object

--- Day 4/object.js
+++ Day 4/object.js
@@ -1,18 +1,46 @@
-const a={
-    id:101,
-    name:"Shatakshi",
-    sal:function(){
-        console.log("My id is: "+this.id);
-        console.log("My name is: "+this.name);
+const a = {
+    id: 101,
+    name: "Shatakshi",
+
+    sal: function() {
+        console.log("My id is: " + this.id);
+        console.log("My name is: " + this.name);
         console.log("My salary is 10000");
     }
 };
 
-let c={
-    age:20,
-    job:"Student",
-    details:function(){
-        console.log("My age is: "+this.age);
-        console.log("My job is: "+this.job);
+a.sal();
+
+
+let c = {
+    age: 20,
+    job: "Student",
+
+    details: function() {
+        console.log("My age is: " + this.age);
+        console.log("My job is: " + this.job);
     }
 };
+
+c.details();
+
+
+const employee = {
+    name: "Rahul",
+    salary: 15000,
+
+    display: function() {
+        console.log("My name is: " + this.name);
+        console.log("My salary is: " + this.salary);
+    },
+
+    increaseSalary: function(amount) {
+        this.salary += amount;
+    }
+};
+
+employee.display();
+
+employee.increaseSalary(5000);
+
+employee.display();
