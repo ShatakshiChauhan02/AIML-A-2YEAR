add6b5996b6c7eb85a79264e0e6d9173ad08779d
ShatakshiChauhan02 <shatakshichauhanabes28@gmail.com>
2026-09-21 15:35:01 +0530

    JS

--- test.js
+++ test.js
@@ -9,3 +9,36 @@ class Employee{
         return this.basicSalary;
     }
 }
+
+//Call Back Function
+function greeUser(name,callback){
+    console.log("Hello "+name);
+    callback();
+}
+
+greeUser("Bob", function(){
+    console.log("Callback function executed.");
+});
+
+console.log("Start ")
+setTimeout(function(){
+    console.log("Task Completed")
+},2000)
+
+console.log("End")
+
+//Synchronus
+
+console.log(10)
+console.log(20)
+console.log(40)
+console.log(50)
+
+//Async
+
+console.log("Start");
+
+setTimeout(function(){
+    console.log("Middle");
+}, 2000);
+console.log("End");
